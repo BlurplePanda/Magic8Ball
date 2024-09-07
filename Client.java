3ce0f86ae9aa8d5009a9f1603cf1af09028ca21e
BlurplePanda <bootham@wgc.school.nz>
2024-09-07 16:35:50 +1200

    fixed String/int issue and added catch

--- Client.java
+++ Client.java
@@ -4,7 +4,7 @@ public class Client {
     public static void main(String[] args) {
         if (args.length != 3) System.exit(1);
         try {
-            Socket s = new Socket(args[0], args[1]);
+            Socket s = new Socket(args[0], Integer.parseInt(args[1]));
             InputStream inputStream = s.getInputStream();
             BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
             StringBuilder response = new StringBuilder("Magic 8 Ball says: ");
@@ -13,6 +13,9 @@ public class Client {
                 response.append(line);
             }
             System.out.println(response);
+        }
+        catch(Exception e) {
+            System.out.println(e);
         }
     }
 }
